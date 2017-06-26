4478a6e31d612f53cd966f0024483c94c7467857
shushanfx <shushanfx@gmail.com>
2017-06-26 10:04:34 +0800

    Add event class

---
+++ src/error.js
@@ -0,0 +1,20 @@
+const ERROR_TYPE = {
+    ERROR_CONNECT: 1, // fire when connect fail.
+    ERROR_READ: 2, // when read with error, such as node not exist, node remove.
+    ERROR_PARSE: 4, // when parse with error, parse the content to a config instance.
+    ERROR_MONITOR: 8, // monitor error, when upload the monitor information.
+    ERROR_HANDLE: 16, // when handle the config instance with error.
+}
+
+class ZKConfigError extends Error {
+    constructor(err, type) {
+        super(err.msg);
+        this.err = err;
+        this.type = type;
+    }
+}
+
+exports = module.exports = ZKConfigError;
+Object.keys(ERROR_TYPE).forEach(key => {
+    ZKConfigError[key] = ERROR_TYPE[key];
+});
