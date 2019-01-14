b4bb91a41c8902348e53d823517501a981b0267b
dengjianxin <dengjianxin@sogou-inc.com>
2019-01-14 12:10:44 +0800

    test:修改测试脚本

--- spec/client/clientSpec.js
+++ spec/client/clientSpec.js
@@ -1,41 +1,52 @@
 var ZKConfig = require("../../index.js");
 
-describe("I have a dream.", function() {
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60 * 1000
+
+describe("I have a dream.", function () {
     var client = null;
     var config = null;
-    beforeAll(function(callback) {
-        client = new ZKConfig.Client("10.110.28.204:2181", {
-            path: "/zkconfig/config/test",
+    var error = null;
+    beforeAll(function (callback) {
+        client = new ZKConfig.Client("10.138.30.208:2181", {
+            path: "/zkconfig/config/gaokao_config_dev",
             monitor: true,
             monitorPath: "/zkconfig/connection",
+            parser: 'json'
         });
-        client.on("connected", function() {
+        client.on("connected", function () {
             console.info("connected");
             callback();
         });
-        client.on("data", function(data) {
+        client.on("data", function (data) {
+            console.info("data");
             config = data;
         });
-        client.on("error", function(err) {
+        client.on("error", function (err) {
             if (err instanceof Error) {
                 console.info(err.message)
             }
+            error = true;
             callback();
         });
         client.connect();
     });
-    it("Yes", function(callback) {
+    it("Yes", function (callback) {
         if (!config) {
-            client.on("data", function(config) {
-                console.info(config.get("name"))
+            if (error) {
+                console.error('Error read.');
                 callback();
-            });
+            } else {
+                client.on("data", function (config) {
+                    console.info("name, " + config.get("name"))
+                    callback();
+                });
+            }
         } else {
             console.info("name, " + config.get("name"))
             callback();
         }
     });
-    afterAll(function() {
+    afterAll(function () {
         client.close();
     });
 })
